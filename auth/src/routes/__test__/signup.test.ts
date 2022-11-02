5e4affd045dfc6af3c0a91f9b0885afdf704dc34
unknown <coolshiv0721@gmail.com>
2022-11-02 18:21:17 +0530

    Changing node env during tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -52,3 +52,11 @@ it('disallows duplicate emails', async () => {
     .send({ email: 'test@test.com', password: 'password' })
     .expect(400);
 });
+
+it('disallows duplicate emails', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
