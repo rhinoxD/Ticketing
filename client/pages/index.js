8b11d594b340b123a3cfea8fd6f08b8f7f8dbe2d
unknown <coolshiv0721@gmail.com>
2022-11-03 18:24:53 +0530

    Fetching data during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
-const LandingPage = ({ color }) => {
-  console.log('In the component', color);
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('On the server!');
+// LandingPage.getInitialProps = async () => {
+//   const response = await axios.get('/api/users/currentuser');
 
-  return { color: 'red' };
-};
+//   return response.data;
+// };
 
 export default LandingPage;
