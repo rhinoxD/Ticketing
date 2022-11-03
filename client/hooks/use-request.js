67eb6a8ab3f2be6539793d664059af1cbdfd6be1
unknown <coolshiv0721@gmail.com>
2022-11-03 18:13:32 +0530

    An onSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
@@ -9,8 +9,13 @@ const useRequest = ({ url, method, body }) => {
       setErrors(null);
       const response = await axios[method](url, body);
 
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
       return response.data;
     } catch (err) {
+      console.log(err);
       setErrors(
         <div className='alert alert-danger'>
           <h4>Oops....</h4>
