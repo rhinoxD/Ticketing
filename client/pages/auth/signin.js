31fef2f899fb95fa4fa13eda3da78edcf7b4fb07
unknown <coolshiv0721@gmail.com>
2022-11-04 13:32:40 +0530

    The sign in form

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className='form-group mt-3'>
+        <label>Email Address</label>
+        <input
+          type='text'
+          className='form-control'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group mt-3'>
+        <label>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary mt-3'>Sign In</button>
+    </form>
+  );
+};
+
+export default signin;
