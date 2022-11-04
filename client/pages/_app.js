860092880b5e89bca66f828f7ea3f16fc976116e
unknown <coolshiv0721@gmail.com>
2022-11-04 18:40:49 +0530

    Issues with custom app getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,8 +5,13 @@ const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-AppComponent.getInitialProps = () => {
-  
-}
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log(data);
+
+  return data;
+};
 
 export default AppComponent;
