33c98c1756e6f06a0dc0de575b67d022fad1835e
unknown <coolshiv0721@gmail.com>
2022-11-04 18:46:05 +0530

    Handling multiple getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,6 +9,11 @@ AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/currentuser');
 
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
   console.log(data);
 
   return data;
