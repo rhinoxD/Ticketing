916801624fd1ec4addbd34e85360ab2cfd49d587
unknown <coolshiv0721@gmail.com>
2022-11-04 13:04:16 +0530

    On the server or the browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,10 +10,15 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
-
-//   return response.data;
-// };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // requests should be made to http://ingress-nginx.ingress...sdfa
+  } else {
+    // we are on the browser
+    // requests can be made with a base url of '' 
+  }
+  return {};
+};
 
 export default LandingPage;
