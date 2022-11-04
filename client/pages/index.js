fcdd7578458492ff53a933125d7c577f103cf0da
unknown <coolshiv0721@gmail.com>
2022-11-04 13:15:44 +0530

    Specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
 
   return <h1>Landing Page</h1>;
 };
@@ -14,9 +14,22 @@ LandingPage.getInitialProps = async () => {
   if (typeof window === 'undefined') {
     // we are on the server
     // requests should be made to http://ingress-nginx.ingress...sdfa
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
   } else {
     // we are on the browser
-    // requests can be made with a base url of '' 
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
   }
   return {};
 };
