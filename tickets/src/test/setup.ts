5e24f6d593002714d0a9f624829dc13aa8d8c35b
unknown <coolshiv0721@gmail.com>
2022-11-06 12:48:19 +0530

    Building a session

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -32,16 +33,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT paylaod. {id, email}
+  const payload = {
+    id: '133v414314d',
+    email: 'test@test.com',
+  };
 
   // Create a JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build session object. {jwt: MY_JWT}
+  const session = { jwt: token };
 
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
   // return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
