a18cbabf95fb36414c25ca08c2d75fe4553c734f
unknown <coolshiv0721@gmail.com>
2022-11-09 18:19:26 +0530

    Event redelivery

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,7 +15,11 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable();
+
   const subscription = stan.subscribe(
     'ticket:created',
     'orders-service-queue-group',
