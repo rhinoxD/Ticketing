72d94d5fb366c6788e5e466b9800c66f6683c750
unknown <coolshiv0721@gmail.com>
2022-11-16 14:46:33 +0530

    Publishing the order cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -7,6 +7,8 @@ import {
 } from '@rxdtickets/common';
 
 import { Order } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -16,7 +18,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -26,6 +28,14 @@ router.delete(
     }
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
