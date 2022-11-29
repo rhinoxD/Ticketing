04376cac55e3215dece49a93086a46d5eaa6d11f
unknown <coolshiv0721@gmail.com>
2022-11-29 15:30:49 +0530

    Testing same-user validation

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -34,3 +34,24 @@ it('returns a 401 when purchasing an order that does not belong to the user', as
     })
     .expect(401);
 });
+
+it('returns a 400 when purchasing a cancelled order', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      orderId: order.id,
+      token: 'asdlkfj',
+    })
+    .expect(400);
+});
